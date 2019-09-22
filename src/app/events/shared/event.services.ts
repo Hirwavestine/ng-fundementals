c64c0dce871497c637e26cac8660dae989035b41
hirwavestine <gilhirwa22@gmail.com>
2019-09-22 19:59:08 +0200

    Adding multiple pages to your App

--- src/app/events/shared/event.services.ts
+++ src/app/events/shared/event.services.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
 @Injectable()
 export class EventService {
-  getEvents() {
+  getEvents() { 
     return EVENTS;
+  }
+  getEvent(id: number) {
+    return EVENTS.find(event => event.id === id);
   }
 }
 const EVENTS = [
